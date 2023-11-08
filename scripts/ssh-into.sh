7042555a69136daa53cb96a6556e44896c2308e5
Mirror <mirror12k@gmail.com>
2023-11-07 20:01:00 -0500

    - added some argument checking to instance name

--- scripts/ssh-into.sh
+++ scripts/ssh-into.sh
@@ -3,6 +3,11 @@ cd "$(dirname "$0")" && cd ..
 
 INSTANCE_NAME=$1
 
+if [ "$INSTANCE_NAME" = "" ]; then
+    echo "[!] missing instance name argument!"
+    exit
+fi
+
 echo "[+] getting instance ip from infrastructure..."
 INSTANCE_ID=$(cat "infrastructure/terraform.tfstate" | jq -r ".outputs.${INSTANCE_NAME}_instance_id.value")
 
