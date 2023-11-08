b4b42415f696a857c181424dae3dd91fa161711b
Mirror <mirror12k@gmail.com>
2023-11-07 19:40:16 -0500

    - fixed package builds for server packages  - changed key generation to use PEM

--- scripts/create-keys.sh
+++ scripts/create-keys.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")" && cd ..
 KEYPATH=$1
 if [[ ! -f ".keys/$KEYPATH" ]]; then
 	mkdir -p .keys/
-	ssh-keygen -t rsa -b 2048 -f ".keys/$KEYPATH" -q -N ""
+	ssh-keygen -m PEM -t rsa -b 2048 -f ".keys/$KEYPATH" -q -N ""
 fi
