9251b8aff5a1086385c897a6cd5a079015cb6542
mirror12k <mirror12k@gmail.com>
2023-11-16 08:55:47 -0500

    - implemented buoy-mine  - improved ssm-into handling  - added uptime alarm, dns query logging, and uptime metrics to bastion template

--- scripts/ssm-into.sh
+++ scripts/ssm-into.sh
@@ -3,13 +3,11 @@ cd "$(dirname "$0")" && cd ..
 
 INSTANCE_NAME=$1
 
-if [ "$INSTANCE_NAME" = "" ]; then
-    echo "[!] missing instance name argument!"
-    exit
-fi
+if [ "$INSTANCE_NAME" = "" ]; then echo "[!] missing instance name argument!"; exit; fi
 
-echo "[+] getting instance ip from infrastructure..."
+echo "[i] getting instance id from infrastructure..."
 INSTANCE_ID=$(cat "infrastructure/terraform.tfstate" | jq -r ".outputs.${INSTANCE_NAME}_instance_id.value")
+if [ "$INSTANCE_ID" = "null" ]; then echo "[!] instance id not found for name '$INSTANCE_NAME'!"; exit; fi
 
 echo "[+] logging in to ssm: $INSTANCE_ID"
-aws ssm start-session --target "$INSTANCE_ID" --region us-east-1 --document-name AWS-StartInteractiveCommand --parameters command="cd /app && sudo su ubuntu"
+aws ssm start-session --target "$INSTANCE_ID" --reason "manual login" --region us-east-1 --document-name AWS-StartInteractiveCommand --parameters command="cd /app && sudo su ubuntu"
